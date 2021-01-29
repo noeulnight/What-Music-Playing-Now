93fa5441694fba22d03ab448e674997e9791a8ab
noeulnight <noeulnight.mail@gmail.com>
2021-01-29 14:08:11 +0900

    noice

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT || 8123
 const YTMD_IP = process.env.YTMD_IP || '192.168.219.127:8303'
 const { createServer } = require('http')
 const path = require('path').resolve()
-const fetch = require('node-fetch')
+const su = require('superagent')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
@@ -11,22 +11,15 @@ const srv = createServer(app)
 app.use(morgan('common'))
 app.get('/', (req, res) => res.sendFile(path + '/page/index.html'))
 
-app.get('/api', (req, res) => {
-  music_data().then((data) => {
-    res.json(data)
-  })
+app.get('/api', async (req, res) => {
+  res.json(await music_data())
 })
 
-function music_data() {
-  return new Promise((resolve) => {
-    fetch(`http://${YTMD_IP}/query`, {method:'GET'}).then(res => res.json()).then(query => {
-      fetch(`http://${YTMD_IP}/query/queue`, {method:'GET'}).then(res => res.json()).then(queue => {
-        return resolve({ query, queue })
-      })
-    }).catch((err) => {
-      return resolve({ status:500, msg:'Service Offline' })
-    })
-  })
+async function music_data() {
+  const query = await su.get(`http://${YTMD_IP}/query`).timeout({deadline:1000}).catch(() => { return { status:500 }})
+  const queue = await su.get(`http://${YTMD_IP}/query/queue`).timeout({deadline:1000}).catch(() => { return { status:500 }})
+  if (query.status === 500) return { status:500, msg:'Service Offline' }
+  return { query, queue }
 }
 
 srv.listen(PORT, () => console.log('http://localhost:' + PORT))
