2c043453ba716a196cb14237df83b57e4ea18172
noeulnight <noeulnight.mail@gmail.com>
2021-01-29 13:53:05 +0900

    sans

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8123
 const YTMD_IP = process.env.YTMD_IP || '192.168.219.127:8303'
 const { createServer } = require('http')
 const path = require('path').resolve()
 const fetch = require('node-fetch')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
-
-const Socket = require('socket.io')
-const SocketClient = require('socket.io-client')
 const srv = createServer(app)
-const wss = Socket(srv)
-const ioClient = SocketClient.io('ws://localhost:9863')
 
+app.use(morgan('common'))
 app.get('/', (req, res) => res.sendFile(path + '/page/index.html'))
-
-setInterval(() => {
-  ioClient.emit('query-player', ('hi'))
-}, 1000)
-ioClient.on("player", (msg) => console.log(msg))
-
-wss.on('connection', (socket) => {
-  setInterval(() => {
-    music_data().then((data) => {
-      socket.emit('tick', data)
-    })
-  }, 500)
-})
 
 app.get('/api', (req, res) => {
   music_data().then((data) => {
@@ -37,8 +21,10 @@ function music_data() {
   return new Promise((resolve) => {
     fetch(`http://${YTMD_IP}/query`, {method:'GET'}).then(res => res.json()).then(query => {
       fetch(`http://${YTMD_IP}/query/queue`, {method:'GET'}).then(res => res.json()).then(queue => {
-        resolve({ query, queue })
+        return resolve({ query, queue })
       })
+    }).catch((err) => {
+      return resolve({ status:500, msg:'Service Offline' })
     })
   })
 }
